5d5486c05e05a93296b48283ae5d89d0ebf42b97
Philip Owusu Asare <philip.asare@amalitech.org>
2022-05-13 16:09:22 +0000

    Setup and vue showing on storyblok

---
+++ src/main.js
@@ -0,0 +1,26 @@
+import { createApp } from 'vue';
+import { StoryblokVue, apiPlugin } from '@storyblok/vue';
+import App from './App.vue';
+
+import Grid from "./components/Grid.vue";
+import Page from "./components/Page.vue";
+import Teaser from "./components/Teaser.vue";
+import Feature from "./components/Feature.vue";
+ 
+const app = createApp(App);
+ 
+app.component("Grid", Grid);
+app.component("Page", Page);
+app.component("Teaser", Teaser);
+app.component("Feature", Feature);
+
+app.use(StoryblokVue, {
+    accessToken: 'ez8HyP0f7TQcxw9AaHHmNgtt',
+    bridge: process.env.NODE_ENV !== 'production', // optimizes by excluding the bridge on production
+    use: [apiPlugin],
+  });
+
+app.mount('#app');
+
+
+
